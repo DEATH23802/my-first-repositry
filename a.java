a0fcdeb0211a756274def5688f1342bbf7173a26
M3tatr00p <52278696+M3tatr00p@users.noreply.github.com>
2022-08-04 15:45:17 -0400

    Update a.java

--- a.java
+++ a.java
@@ -4,7 +4,7 @@ class a
 public static void main(String args[])
 {
 int x,y,z;
-System.out.println("enter two values");
+System.out.println("enter the two values");
 z=x+y;
 System.out.println("add is"+z);
 }
